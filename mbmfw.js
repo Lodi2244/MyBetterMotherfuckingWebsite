5570376adf930d645fdef66d9b87ba128d26416e
Elo <elodieletellier1@gmail.com>
2018-09-25 13:40:08 +0200

    add js file

---
+++ mbmfw.js
@@ -0,0 +1,11 @@
+
+var popUp = "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"
+		
+setTimeout(function(){
+	alert(popUp);
+		if (confirm("Cliquez sur Ok pour être redirigé vers xxxvidsxxx")){
+			window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+		}
+}, 10000);
+	
+
